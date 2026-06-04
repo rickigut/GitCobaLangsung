9630f1fbed52fd3383c16410418524e4c616a279
rickigut <rickichenel@gmail.com>
2026-06-04 11:00:53 +0700

    add text hallo bang

---
+++ GitCobaLangsung/itulah-pokoknya.swift
@@ -0,0 +1,19 @@
+//
+//  itulah-pokoknya.swift
+//  GitCobaLangsung
+//
+//  Created by Ricki Darmawan Putra on 04/06/26.
+//
+
+import SwiftUI
+
+struct itulah_pokoknya: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, bang!")
+    }
+}
+
+#Preview {
+    itulah_pokoknya()
+}
